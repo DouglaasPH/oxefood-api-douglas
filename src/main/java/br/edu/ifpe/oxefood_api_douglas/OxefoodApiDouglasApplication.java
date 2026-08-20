5900beed2718a8afd52f69fe15934cb4cb7199ab
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-08-20 17:15:04 -0300

    add java 21

---
+++ src/main/java/br/edu/ifpe/oxefood_api_douglas/OxefoodApiDouglasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.oxefood_api_douglas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiDouglasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiDouglasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/oxefood_api_douglas/OxefoodApiDouglasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.oxefood_api_douglas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiDouglasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
